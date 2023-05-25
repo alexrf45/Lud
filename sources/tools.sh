81b0fd49b4ff277e25ad42e464d6a26a845145db
alexrf45 <fonalex45@gmail.com>
2023-05-24 21:11:13 -0400

    testing new go install function

--- sources/tools.sh
+++ sources/tools.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 function install_go() {
-	wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz &&
+	file=$(curl -Ls https://go.dev/dl | grep -oP '(?<=<a class="download downloadBox" href="/dl/).*linux.*(?=">)')
+
+	version=$(echo "$file" | grep -oP '(?<=go).*(?=.linux)')
+
+	wget "https://go.dev/dl/$file" -q --show-progress &&
 		rm -rf /usr/local/go &&
-		sudo tar -C /usr/local/bin -xzf go1.20.3.linux-amd64.tar.gz &&
-		rm go1.20.3.linux-amd64.tar.gz
+		sudo tar -C /usr/local/bin -xzf "$file" &&
+		rm "$file"
 }
 
 function waybackurls_install() {
